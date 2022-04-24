c3737f8e55d43c1ea8d02149befd49d21cf2873c
RCASTRO-TABLET\RicardoCastro <cardo88@gmail.com>
2022-04-24 20:26:26 -0300

    codigo original con test realizados

--- test/test.cs
+++ test/test.cs
@@ -1,18 +1,38 @@
 using NUnit.Framework;
-using DataFormat;
+using DateFormat;
 
-namespace DataFormat.test;
-
-public class Tests
+namespace DataFormat.test
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class Tests
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [Test]
+        public void Test1()
+        {
+            const string datoenviado = "10/11/1977";
+            const string datoesperado = "1977-11-10";
+            Assert.AreEqual(Program.ChangeFormat(datoenviado),datoesperado);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            const string datoenviado = "01/01/0011";
+            const string datoesperado = "0011-01-01";
+            Assert.AreEqual(Program.ChangeFormat(datoenviado),datoesperado);
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            const string datoenviado = "dd/mm/aaaa";
+            const string datoesperado = "aaaa-mm-dd";
+            Assert.AreEqual(Program.ChangeFormat(datoenviado),datoesperado);
+        }
     }
 }
