02796b26644d013dc141dd24b214159e873fa32d
RCASTRO-TABLET\RicardoCastro <cardo88@gmail.com>
2022-04-24 19:34:23 -0300

    preparando proyecto

---
+++ test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using DataFormat;
+
+namespace DataFormat.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
